1a5d7f105b692e5ebe848a587482af1702bdad9b
kirankumargunisetty <gunisettykirankumar7@gmail.com>
2026-06-19 11:24:57 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-AMI_ID=ami-0220d79f3f480ecf5
-ZONE_ID=Z03248292KWELLMNVDW64
-DOMAIN_NAME=devopskiran.online
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z03248292KWELLMNVDW64"
+DOMAIN_NAME="devopskiran.online"
 
 for instance in $@
 do
@@ -11,7 +11,7 @@ do
         --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
         --security-groups "roboshop-common" "roboshop-$instance" \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$instance"}]' \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
     )
@@ -22,10 +22,38 @@ do
          --query 'Reservations[*].Instances[*].PublicIpAddress' \
          --output text
          )
+         R53_RECORD="DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
          --query 'Reservations[*].Instances[*].PrivateIpAddress' \
          --output text
          )
+         R53_RECORD="$instance.DOMAIN_NAME"
     fi
+    echo "R53_RECORD: R53_RECORD"
+
+    #### Updating R53 Record ####
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+        {
+            "Comment": "Update A record to new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$R53_RECORD'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    '
+    
 done
